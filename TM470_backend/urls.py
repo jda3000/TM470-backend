3b9372ed270e591c155b864f5f93aa1afbf8b120
James Alexander <mrrewire@gmail.com>
2021-12-28 23:45:12 +0000

    import fix

--- TM470_backend/urls.py
+++ TM470_backend/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 from .settings.jwt_token import CustomTokenObtainPairView
