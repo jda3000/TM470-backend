690c9538a1335f83ecbcb02ea29ef7fe6858d04a
James Alexander <mrrewire@gmail.com>
2021-12-29 00:09:46 +0000

    postgis database extention

--- beats/models.py
+++ beats/models.py
@@ -1,16 +1,7 @@
-from django.contrib.postgres.operations import CreateExtension
-from django.db import migrations
 from django.contrib.gis.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
-
-class Migration(migrations.Migration):
-
-    operations = [
-        CreateExtension('postgis'),
-    ]
 
 
 class Beat(models.Model):
