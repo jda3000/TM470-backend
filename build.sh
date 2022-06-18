61b81fae2c05161641bca593ce56d1ba0f81b210
James Alexander <mrrewire@gmail.com>
2022-06-18 17:21:29 +0100

    aws static files admin

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # exit on error
 set -o errexit
 python -m pip install -r requirements.txt
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 python manage.py migrate
