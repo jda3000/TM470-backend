966cb5b41231beb5a22ce9a48099dc77af2f47e0
James Alexander <mrrewire@gmail.com>
2022-07-07 21:56:04 +0100

    db

--- beats/models/beat.py
+++ beats/models/beat.py
@@ -18,14 +18,13 @@ User = get_user_model()
 class Beat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
-    route = models.LineStringField(blank=True, null=True)
+    route = models.LineStringField(blank=True, null=True, help_text='Route data')
     date_created = models.DateTimeField(auto_now_add=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(null=True, blank=True) # allow blank end time as may want to save beat before finishing it (i.e. to resume at a later time/date)
     litter_collected_amount = models.PositiveSmallIntegerField(null=True, blank=True)
-    private = models.BooleanField(default=False)
-    start_point = models.PointField(null=True, blank=True)
-
+    private = models.BooleanField(default=False, help_text='Route is private')
+    start_point = models.PointField(null=True, blank=True, help_text='start geo location of the route')
 
     def __str__(self):
         return f'{self.description}'
